967f6d4547a283b018743d6273ef806dc79a414a
dev01997D <devnandan01997@gmail.com>
2020-09-24 23:55:33 +0530

    Started: Welcome message added

---
+++ snakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder;
+
+public class snakeAndLadder {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Welcome statement
+		System.out.println("Welcome to Snake and Ladder Simulator");
+	}
+
+}
