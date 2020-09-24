c2f40138f14f507b712f5eee831863a5468db3fe
dev01997D <devnandan01997@gmail.com>
2020-09-25 00:41:51 +0530

    Added: Finding total number of dice rolled feature added

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -8,35 +8,45 @@ public class snakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		//Variables
 		int player_Position=0;
+		int counter=0;
 
 		//Welcome statement
 		System.out.println("Welcome to Snake and Ladder Simulator");
 
 		while(player_Position<100) {
+			counter+=1;
 			//computation
 			int numberOfMove=(int) (Math.floor(Math.random()*10)%7);
+			//System.out.println(numberOfMove);
+
 			int optionOfPlay=(int) (Math.floor(Math.random()*10)%3);
 
 			switch(optionOfPlay) {
 			case noPlay:
 				player_Position=player_Position;
+				System.out.println(player_Position);
 				break;
 			case ladder:
 				if (player_Position+numberOfMove>100)
 					player_Position=player_Position;
 				else
 					player_Position+=numberOfMove;
+				System.out.println(player_Position);
 				break;
 			case snake:
 				if (player_Position-numberOfMove>=0)
 					player_Position-=numberOfMove;
 				else
 					player_Position=0;
+				System.out.println(player_Position);
 				break;
 			}
 		}
+		System.out.println("Total number of times dice was rolled :"+counter);
+		System.out.println("Finally player is at position :"+player_Position);
 	}
 
 }
